630c96bc74d5e911b7f447298349d9e0734a1698
Kathyslate <mercyoyetunde98@gmail.com>
2024-03-09 11:33:04 -0800

    ertyu

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,41 @@
-import uuid
 from datetime import datetime
+from uuid import uuid4
+import models
+from models import storage
 
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    """base model"""
+    def __init__(self, *args, **kwargs):
+        """ Construct now with kwargs """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'created_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if 'id' not in kwargs.keys():
+                    self.id = str(uuid4())
+                if 'created_at' not in kwargs.keys():
+                    self.created_at = datetime.now()
+                if 'updated_at' not in kwargs.keys():
+                    self.updated_at = datetime.now()
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
 
     def save(self):
+        """function to save"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
+        """self to dict"""
         data = self.__dict__.copy()
         data['created_at'] = self.created_at.isoformat()
         data['updated_at'] = self.updated_at.isoformat()
@@ -19,4 +43,5 @@ class BaseModel:
         return data
 
     def __str__(self):
+        """return"""
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
