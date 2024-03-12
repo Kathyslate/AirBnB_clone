13e8369b6a87c1c0d3083618882c1ff8f4aa816f
Emeraldolaitan1220 <smallangel2002@gmail.com>
2024-03-12 12:07:28 -0700

    making neccesary corrections

---
+++ models/base_model.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" Class BaseModel """
+from datetime import datetime
+from uuid import uuid4
+import models
+
+
+class BaseModel:
+    """base model"""
+    def __init__(self, *args, **kwargs):
+        """ Construct now with kwargs """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'created_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if 'id' not in kwargs.keys():
+                    self.id = str(uuid4())
+                if 'created_at' not in kwargs.keys():
+                    self.created_at = datetime.now()
+                if 'updated_at' not in kwargs.keys():
+                    self.updated_at = datetime.now()
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+
+    def save(self):
+        """function to save"""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """self to dict"""
+        data = self.__dict__.copy()
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        data['__class__'] = self.__class__.__name__
+        return data
+
+    def __str__(self):
+        """return"""
+        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
